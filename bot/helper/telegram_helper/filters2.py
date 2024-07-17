f1185d0c4493e7be4ca0635acd88e83c20aa5ac0
krishd9895 <105974502+krishd9895@users.noreply.github.com>
2024-07-17 19:38:30 +0530

    Update filters2.py
    Signed-off-by: krishd9895 <105974502+krishd9895@users.noreply.github.com>

--- bot/helper/telegram_helper/filters2.py
+++ bot/helper/telegram_helper/filters2.py
@@ -1,21 +1,23 @@
 from pyrogram.filters import create
 from bot import OWNER_ID, user_data
 
-
 class CustomFilters:
-    async def custom_owner_filter(self, client, update):
+    @staticmethod
+    async def custom_owner_filter(client, update):
         uid = update.from_user.id or update.sender_chat.id
         return uid == OWNER_ID
 
     owner_filter = create(custom_owner_filter)
 
-    async def custom_chat_filter(self, client, update):
+    @staticmethod
+    async def custom_chat_filter(client, update):
         chat_id = update.chat.id
         return chat_id in user_data and user_data[chat_id].get("is_auth", False)
 
     chat_filter = create(custom_chat_filter)
 
-    async def custom_user_filter(self, client, update):
+    @staticmethod
+    async def custom_user_filter(client, update):
         uid = update.from_user.id or update.sender_chat.id
         return (
             uid == OWNER_ID
@@ -28,7 +30,8 @@ class CustomFilters:
 
     user_filter = create(custom_user_filter)
 
-    async def custom_sudo_filter(self, client, update):
+    @staticmethod
+    async def custom_sudo_filter(client, update):
         uid = update.from_user.id or update.sender_chat.id
         return bool(uid == OWNER_ID or uid in user_data and user_data[uid].get("is_sudo"))
 
